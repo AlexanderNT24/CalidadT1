9b9769f45f15637664a405b8f414b27ddc319053
Alexander Nureña <89727568+AlexanderNT24@users.noreply.github.com>
2022-09-09 08:50:52 -0500

    agrgue mas pruebas libros

--- CalidadT1/PruebaLibros/UnitTest1.cs
+++ CalidadT1/PruebaLibros/UnitTest1.cs
@@ -105,5 +105,100 @@ namespace PruebaLibros
             var precio = tienda.GetTotal();
             Assert.AreEqual(18,precio);
         }
+        [Test]
+        public void Caso06Libros()
+        {
+            var tienda = new BookStore();
+            var libro = new Book();
+            libro.Name = "Libro4";
+            libro.Price = 2;
+            tienda.AddToCart(libro);
+            var libro2 = new Book();
+            libro2.Name = "Libro5";
+            libro2.Price = 2;
+            tienda.AddToCart(libro2);
+            var libro3 = new Book();
+            libro3.Name = "Libro6";
+            libro3.Price = 3;
+            tienda.AddToCart(libro3);
+            var precio = tienda.GetTotal();
+            Assert.AreEqual(7, precio);
+        }
+        [Test]
+        public void Caso07Libros()
+        {
+            var tienda = new BookStore();
+            var libro = new Book();
+            libro.Name = "Libro1";
+            libro.Price = 1;
+            tienda.AddToCart(libro);
+            var libro2 = new Book();
+            libro2.Name = "Libro2";
+            libro2.Price = 1;
+            tienda.AddToCart(libro2);
+            var libro3 = new Book();
+            libro3.Name = "Libro3";
+            libro3.Price = 2;
+            tienda.AddToCart(libro3);
+            var precio = tienda.GetTotal();
+            Assert.AreEqual(4, precio);
+        }
+        [Test]
+        public void Caso08Libros()
+        {
+            var tienda = new BookStore();
+            var libro = new Book();
+            libro.Name = "Libro1";
+            libro.Price = 18;
+            tienda.AddToCart(libro);
+            var libro2 = new Book();
+            libro2.Name = "Libro1";
+            libro2.Price = 18;
+            tienda.AddToCart(libro2);
+            var libro3 = new Book();
+            libro3.Name = "Libro2";
+            libro3.Price = 3;
+            tienda.AddToCart(libro3);
+            var precio = tienda.GetTotal();
+            Assert.AreEqual(37.2, precio);
+        }
+        [Test]
+        public void Caso09Libros()
+        {
+            var tienda = new BookStore();
+            var libro = new Book();
+            libro.Name = "Libro1";
+            libro.Price = 20;
+            tienda.AddToCart(libro);
+            var libro2 = new Book();
+            libro2.Name = "Libro1";
+            libro2.Price = 20;
+            tienda.AddToCart(libro2);
+            var libro3 = new Book();
+            libro3.Name = "Libro2";
+            libro3.Price = 10;
+            tienda.AddToCart(libro3);
+            var precio = tienda.GetTotal();
+            Assert.AreEqual(48, precio);
+        }
+        [Test]
+        public void Caso10Libros()
+        {
+            var tienda = new BookStore();
+            var libro = new Book();
+            libro.Name = "Libro1";
+            libro.Price = 8;
+            tienda.AddToCart(libro);
+            var libro2 = new Book();
+            libro2.Name = "Libro1";
+            libro2.Price = 8;
+            tienda.AddToCart(libro2);
+            var libro3 = new Book();
+            libro3.Name = "Libro2";
+            libro3.Price = 10;
+            tienda.AddToCart(libro3);
+            var precio = tienda.GetTotal();
+            Assert.AreEqual(25.2, precio);
+        }
     }
 }
